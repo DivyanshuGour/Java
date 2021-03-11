3c2bf8bb12fb3827cce940ee22ade1181f439279
DivyanshuGour <78845422+DivyanshuGour@users.noreply.github.com>
2021-03-11 22:34:15 +0530

    Add files via upload

--- FrameDemo.java
+++ FrameDemo.java
@@ -1,21 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package framedemo;
 
-/**
- *
- * @author gours
- */
+import java.awt.Color;
+import java.awt.Frame;
+
 public class FrameDemo {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+       Frame frame = new Frame("Demo Frame");
+        frame.setVisible(true);
+        frame.setSize(400, 400);
+        frame.setBackground(Color.yellow);
+  
     }
     
 }
